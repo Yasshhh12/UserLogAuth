01076daa6877f32c6759de481a1cd66296259fc2
Yasshhh12 <prasyast152@gmail.com>
2024-07-22 16:26:39 +0530

    Created the database

---
+++ back-end/db/dbConnection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async()=>{
+    try{
+        await mongoose.connect('mongodb://localhost:27017/mydatabase');
+        console.log('Connected to mongoDB');
+    }
+    catch{
+        console.log('error');
+    }
+}
+
+module.exports = connectDB;
