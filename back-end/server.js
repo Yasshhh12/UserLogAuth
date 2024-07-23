ae582d9ddf553d5b896d1715dbd944b6fdf67771
Yasshhh12 <prasyast152@gmail.com>
2024-07-23 20:35:07 +0530

    Created the resgistration of the user and connected to db

--- back-end/server.js
+++ back-end/server.js
@@ -8,6 +8,20 @@ connectDB();
 
 app.use(express.json());
 
+//registration
+app.post('/register',async(req,res)=>{
+    try {
+        const {username,password} = req.body;
+        console.log(req.body);
+        const user = new User({username,password});
+        await user.save();
+        res.status(201).json({message:'Registration Succesful'});
+    } 
+    catch (error) {
+        res.status(500).json({error:'Registration failed'});  
+    }
+})
+
 
 app.listen(port,()=>{
     console.log("Server is Listening");
