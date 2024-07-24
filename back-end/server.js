044872d0703a55c770994830f55d3be080dfe9de
Yasshhh12 <prasyast152@gmail.com>
2024-07-24 20:36:50 +0530

    established the backend connection

--- back-end/server.js
+++ back-end/server.js
@@ -6,6 +6,7 @@ const User = require('./db/user');
 
 connectDB();
 
+//middleware
 app.use(express.json());
 
 //registration
@@ -15,10 +16,25 @@ app.post('/register',async(req,res)=>{
         console.log(req.body);
         const user = new User({username,password});
         await user.save();
-        res.status(201).json({message:'Registration Succesful'});
+        res.status(201).json({message:'Registration Successful'});
     } 
     catch (error) {
         res.status(500).json({error:'Registration failed'});  
+    }
+})
+
+app.post('/login',async(req,res)=>{
+    try {
+        const {username,password} = req.body;
+        const user = await User.findOne({username});
+
+        if(!user){
+            return res.status(401).json({error:'Invalid username or Password'});
+        }
+        res.status(200).json({message:'Login Successful'});
+
+    } catch (error) {
+        res.status(500).json({error:'Login failed'});
     }
 })
 
