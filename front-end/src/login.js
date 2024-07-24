28c987eb9b291b1ff23ee62e5c326468e8668b19
Yasshhh12 <prasyast152@gmail.com>
2024-07-24 20:49:20 +0530

    Created the login Page

--- front-end/src/login.js
+++ front-end/src/login.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const login = () => {
+const Login = () => {
+
+  const [LoginData,setLoginData] = useState({
+    username:'',
+    password:'',
+  })
+
+  const handleLoginChange = (e)=>{
+    const {name,value} = e.target; 
+    setLoginData((prevData)=>({
+      ...prevData,
+      [name] : value
+    }))
+  }
+
   return (
     <div>
-        <h1>Hola</h1>
+        <h1>Login Page</h1>
+        <form>
+          Username:
+          <input 
+          type='text' 
+          name='username'
+          placeholder='Username' 
+          required
+          value={LoginData.username}
+          onChange={handleLoginChange}
+          />
+          <br/>
+          Password:
+          <input 
+          type='password' 
+          name='password'
+          placeholder='Password' 
+          required
+          value={LoginData.password}
+          onChange={handleLoginChange}
+          />
+        </form>
     </div>
   )
 }
 
-export default login
+export default Login
