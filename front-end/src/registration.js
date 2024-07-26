1e39ffcaefa7a9f89c2a1ad682f5fa1704226a6b
Yasshhh12 <prasyast152@gmail.com>
2024-07-26 17:14:29 +0530

    Successful registration is regidtered

--- front-end/src/registration.js
+++ front-end/src/registration.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const registration = () => {
+const Registration = () => {
+
+  const [registrationData,setRegistrationData] = useState({
+    username:'',
+    password:'',
+  })
+
+  const handleRegisterChange = (e)=>{
+    const {name,value} = e.target;
+    setRegistrationData((prevData)=>({
+      ...prevData,
+      [name]:value,
+    }))
+  }
+
+  const handleRegisterSubmit = async(e)=>{
+    e.preventDefault();
+
+    try{
+      const response = await axios.post('http://localhost:8000/register',registrationData);
+      console.log(response.data);
+    }
+    catch(error){
+      console.log(error);
+    }
+
+    setRegistrationData({
+      username:'',
+      password:'',
+    })
+
+  }
+  
   return (
-    <div>registration form</div>
+    <div>
+      <h1>Registration form</h1>
+      <form onSubmit={handleRegisterSubmit}>
+        Username:
+        <input 
+        type='text'
+        placeholder='Enter Username'
+        name='username'
+        value={registrationData.username}
+        onChange={handleRegisterChange}
+        required
+        />
+        <br/>
+        Password:
+        <input 
+        type='password'
+        placeholder='Enter Password'
+        name='password'
+        value={registrationData.password}
+        onChange={handleRegisterChange}
+        required
+        />
+        <br/>
+        <button type='submit'>Register</button>
+        <br/>
+        <p>
+          Already Registerd?
+          <Link to='/login'>Login Here</Link>
+        </p>
+      </form>
+    </div>
   )
 }
 
-export default registration
+export default Registration
